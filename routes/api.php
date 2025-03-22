dec7933c083be6c626cd0be7ac9de6e2ce218500
Kevin Misigaro <kevin@databalk.nu>
2025-03-22 12:06:07 +0300

    added missing providers

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ use App\Http\Controllers\ForumController;
 use App\Http\Controllers\SecondForumController;
 use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\SubscriptionController;
-use L5Swagger\Http\Controllers\SwaggerController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
