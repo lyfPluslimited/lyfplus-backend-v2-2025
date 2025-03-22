cb23e2340af173b99695df3b1eabeb0cf308acfc
Kevin Misigaro <kevin@databalk.nu>
2025-03-22 12:35:30 +0300

    fixed imports in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QrCodeController;
+use App\Http\Controllers\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,21 +10,20 @@ use App\Http\Controllers\QrCodeController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group that
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Home route
+Route::get('/', fn () => view('welcome'));
+
+// Registration routes
+Route::controller(RegistrationController::class)->group(function () {
+    Route::post('savepatient', 'createUserFromInvitation')->name('savePatient');
+    Route::get('successfulInvitation', 'showInvitationSuccessPage');
+    Route::get('invitation/{hash}', 'invitationConfirmation');
 });
 
-Route::post('savepatient', 'RegistrationController@createUserFromInvitation')->name('savePatient');
-
-Route::get('docprofile/{id}',[QrCodeController::class,'readQRcode']);
-
-Route::get('successfulInvitation', 'RegistrationController@showInvitationSuccessPage');
-
-Route::get('invitation/{hash}', 'RegistrationController@invitationConfirmation');
-
-Route::post('savepatient', 'RegistrationController@createUserFromInvitation');
+// QR Code route
+Route::get('docprofile/{id}', [QrCodeController::class, 'readQRcode']);
